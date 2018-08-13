e85eccaf381711fac97eda80e0ddd851cfd30e31
Mentor Gashi <mentorgashi@live.com>
2018-08-14 01:40:24 +0200

    Github relase object retreival through helper

--- .cronjob/update.php
+++ .cronjob/update.php
@@ -2,10 +2,7 @@
 
 /* Time after time checkcing the api version for automatic updates */
 if( config('api.autoUpdates')==true ) {
-	$opts = array('http' => array( "method" => "GET", "header" => array("User-Agent: PHP")));
-	$context = stream_context_create($opts);
-	$latestVersionObject = file_get_contents(config("api.releaseUrl"), false, $context);
-	$latestVersionObject = json_decode($latestVersionObject);
+	$latestVersionObject = apiLatestVersionObject();
 	if(config('api.version') != $latestVersionObject->tag_name) {
 		echo "Downloading new version";
 		file_put_contents("latest-release.zip", file_get_contents($latestVersionObject->zipball_url, false, $context));
