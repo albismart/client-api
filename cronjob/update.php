3290653b47f57fdfe681766721fb11a60749c80b
Mentor Gashi <mentorgashi@live.com>
2018-08-13 16:15:15 +0200

    Cronjob api update task

--- cronjob/update.php
+++ cronjob/update.php
@@ -7,7 +7,7 @@ include_once $currentPath . "/init.php";
 if( config('api.autoUpdates')==true ) {
 	$opts = array('http' => array( "method" => "GET", "header" => array("User-Agent: PHP")));
 	$context = stream_context_create($opts);
-	$latestVersionObject = file_get_contents("https://api.github.com/repos/albismart/client-api/releases/latest", false, $context);
+	$latestVersionObject = file_get_contents(config("api.releaseUrl"), false, $context);
 	$latestVersionObject = json_decode($latestVersionObject);
 	if(config('api.version') != $latestVersionObject->tag_name) {
 		echo "Downloading new version";
