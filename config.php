a5035e5f41be1508aac6c11806a5d3b65c1bf331
Mentor Gashi <mentorgashi@live.com>
2018-08-13 16:14:44 +0200

    Configuration basics

--- config.php
+++ config.php
@@ -1,5 +1,19 @@
 <?php
 
 return [
-	// Config values here
+	"api" => array(
+		"key" => "cbe532cdb56367274273b2b36a1420e2b939e72c",
+		"version" => "v0.1",
+		"autoUpdates" => true,
+		"releaseUrl" => "https://api.github.com/repos/albismart/client-api/releases/latest"
+	),
+	"database" => array(
+		"host" => "localhost",
+		"port" => 3306,
+		"name" => "albismart",
+		"username" => "root",
+		"password" => 12345678
+	)
 ];
+
+?>
