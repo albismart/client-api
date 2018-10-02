4b069f3e82ccd298ea44cdf7523202b9fe1c8dfb
= <=>
2018-10-02 06:31:46 -0400

    Added mysql port and checking all ports status

--- info/index.php
+++ info/index.php
@@ -15,10 +15,17 @@ $linuxInfo = array(
 	"sftp" => (getservbyport(22, "tcp")=="ssh") ? true : false, // Checking if port is default
 	"telnet" => (getservbyport(23, "tcp")=="telnet") ? true : false, // Checking if port is default
 	"snmp" => (getservbyport(161, "udp")=="snmp") ? true : false, // Checking if port is default
+	"mysql" => (getservbyport(3306, "tcp")=="mysql") ? true : false, // Checking if port is default
 	"address" => mapNetworkInfo($networkInfo, "address"), 
 	"subnet" => mapNetworkInfo($networkInfo, "subnet"), 
 	"gateway" => mapNetworkInfo($networkInfo, "gateway"),
 );
+
+if(!is_resource(@fsockopen("127.0.0.1", 21))) { $linuxInfo['ftp'] = false; }
+if(!is_resource(@fsockopen("127.0.0.1", 22))) { $linuxInfo['sftp'] = false; }
+if(!is_resource(@fsockopen("127.0.0.1", 23))) { $linuxInfo['telnet'] = false; }
+if(!is_resource(@fsockopen("127.0.0.1", 161))) { $linuxInfo['snmp'] = false; }
+if(!is_resource(@fsockopen("127.0.0.1", 3306))) { $linuxInfo['mysql'] = false; }
 
 function mapNetworkInfo($networkInfo, $info) {
 	$map = array('address'=>'inet addr:', 'subnet'=>'Mask:', 'gateway'=>'Bcast:');
