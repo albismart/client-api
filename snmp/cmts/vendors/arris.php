8acad25431add390e4a96ecf027ac90292dd5252
Mentor Gashi <mentorgashi@live.com>
2018-10-02 11:08:23 -0400

    Adding general mibs to all

--- snmp/cmts/vendors/arris.php
+++ snmp/cmts/vendors/arris.php
@@ -4,6 +4,11 @@
 $arris = "1.3.6.1.4.1.4998";
 
 return array(
+	"cmtsName" => "1.3.6.1.2.1.1.5.0", 
+	"cmtsDescription" => "1.3.6.1.2.1.1.1.0",
+	"cmtsUptime" => "1.3.6.1.2.1.1.3.0",
+
+	/* Arris Specified */
 	"cmtsCpuUsage" => "{$arris}.1.1.5.3.1.1.1.8.2",
 	"cmtsTemperatureIn" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
 	"cmtsTemperatureOut" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
--- snmp/cmts/vendors/cisco.php
+++ snmp/cmts/vendors/cisco.php
@@ -4,6 +4,11 @@
 $cisco = "1.3.6.1.4.1.9";
 
 return array(
+	"cmtsName" => "1.3.6.1.2.1.1.5.0", 
+	"cmtsDescription" => "1.3.6.1.2.1.1.1.0",
+	"cmtsUptime" => "1.3.6.1.2.1.1.3.0",
+	
+	/* Cisco Specified */
 	"cmtsCpuUsage" => "{$cisco}.2.1.57.0",
 	"cmtsTemperatureIn" => "{$cisco}.9.13.1.3.1.3.1",
 	"cmtsTemperatureOut" => "{$cisco}.9.13.1.3.1.3.2",
