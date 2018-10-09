79b888750e910fd896e10b6431f561a9871d7335
Mentor Gashi <mentorgashi@live.com>
2018-10-09 14:51:26 +0200

    AlbiSmarts initial MIB

---
+++ snmp/general-mib.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+All devices Object ID's from general Mib Tree.
+https://kb.paessler.com/en/topic/653-how-do-snmp-mibs-and-oids-work
+A detailed answer on how SNMP and its MIBs and OIDs work.
+*/
+
+$arris = "1.3.6.1.2.1.1";
+
+return array(
+	"name" => "1.3.6.1.2.1.1.5.0", 
+	"description" => "1.3.6.1.2.1.1.1.0",
+	"uptime" => "1.3.6.1.2.1.1.3.0",
+);
+
+?>
