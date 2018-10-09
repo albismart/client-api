28d3b71890222ad14d400a989c18bce930aa5dac
Mentor Gashi <mentorgashi@live.com>
2018-10-10 00:12:54 +0200

    Removed general oid from vendor MIBS

--- snmp/cmts/vendors/arris.php
+++ snmp/cmts/vendors/arris.php
@@ -4,12 +4,9 @@
 $arris = "1.3.6.1.4.1.4998";
 
 return array(
-	"cmtsName" => "1.3.6.1.2.1.1.5.0", 
-	"cmtsDescription" => "1.3.6.1.2.1.1.1.0",
-	"cmtsUptime" => "1.3.6.1.2.1.1.3.0",
-
-	/* Arris Specified */
-	"cmtsCpuUsage" => "{$arris}.1.1.5.3.1.1.1.8.2",
-	"cmtsTemperatureIn" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
-	"cmtsTemperatureOut" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
+	"cpuUsage" => "{$arris}.1.1.5.3.1.1.1.8.2",
+	"temperatureIn" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
+	"temperatureOut" => "{$arris}.1.1.10.1.4.2.1.29.1.20",
 );
+
+?>
--- snmp/cmts/vendors/cisco.php
+++ snmp/cmts/vendors/cisco.php
@@ -4,12 +4,9 @@
 $cisco = "1.3.6.1.4.1.9";
 
 return array(
-	"cmtsName" => "1.3.6.1.2.1.1.5.0", 
-	"cmtsDescription" => "1.3.6.1.2.1.1.1.0",
-	"cmtsUptime" => "1.3.6.1.2.1.1.3.0",
-	
-	/* Cisco Specified */
-	"cmtsCpuUsage" => "{$cisco}.2.1.57.0",
-	"cmtsTemperatureIn" => "{$cisco}.9.13.1.3.1.3.1",
-	"cmtsTemperatureOut" => "{$cisco}.9.13.1.3.1.3.2",
+	"cpuUsage" => "{$cisco}.2.1.57.0",
+	"temperatureIn" => "{$cisco}.9.13.1.3.1.3.1",
+	"temperatureOut" => "{$cisco}.9.13.1.3.1.3.2",
 );
+
+?>
