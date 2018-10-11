2ff6a2d4386c77e05a971e5bea5884d9cf645d73
Mentor Gashi <mentorgashi@live.com>
2018-10-11 22:54:43 +0200

    Interfaces list of CMTS

--- views/tabs/index/cmts-insight.php
+++ views/tabs/index/cmts-insight.php
@@ -1,8 +1,17 @@
 <?php
+
+// CMTS Hostname from previous form
 $hostname = (isset($_GET['hostname'])) ? $_GET['hostname'] : null;
+
+// CMTS Info API Call example
 $cmtsInfoUrl = apiRootURL("/snmp/cmts?hostname=" . $hostname);
 $cmtsInfo = file_get_contents($cmtsInfoUrl);
 $cmtsInfo = json_decode($cmtsInfo);
+
+// CMTS Interfaces API Call example
+$cmtsInterfacesUrl = apiRootURL("/snmp/cmts?hostname=" . $hostname . "&action=interfaces");
+$cmtsInterfaces = file_get_contents($cmtsInterfacesUrl);
+$cmtsInterfaces = json_decode($cmtsInterfaces);
 
 $jsCmtsInfoUrl = "http://".$_SERVER['HTTP_HOST'] . strstr(str_replace("/index.php","",$_SERVER['REQUEST_URI']),"?",true);
 $jsCmtsInfoUrl.= "snmp/cmts?hostname=" . $hostname;
@@ -10,14 +19,15 @@ $jsCmtsInfoUrl.= "snmp/cmts?hostname=" . $hostname;
 <div class="columns">
 	<div class="col-2">&nbsp;</div>
 	<div class="col-8">
-		<table class="intro">
-			<thead>
+		<table class="card">
+			<thead class="header">
 				<tr>
 					<th>Name</th>
 					<th>Uptime</th>
 					<th>CPU Usage</th>
 					<th>Temperature IN</th>
 					<th>Temperature Out</th>
+					<th>Total interfaces</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -35,18 +45,42 @@ $jsCmtsInfoUrl.= "snmp/cmts?hostname=" . $hostname;
 					<td><span id="cmtsCpuUsage"><?php echo $cmtsInfo->cpuUsage; ?></span>%</td>
 					<td><span id="cmtsTemperatureIn"><?php echo $cmtsInfo->temperatureIn; ?></span> °C</td>
 					<td><span id="cmtsTemperatureOut"><?php echo $cmtsInfo->temperatureOut; ?></span> °C</td>
+					<td><span id="cmtsCountInterfaces"><?php echo $cmtsInfo->countInterfaces; ?></span></td>
 				</tr>
 				<tr id="cmtsDetails" style="display:none">
-					<td colspan="5" style="border-top: 1px solid #ccc;padding: 20px;line-height:30px">
+					<td colspan="6" style="border-top: 1px solid #ccc;padding: 20px;line-height:30px">
 						<?php
 							echo str_replace(",","<br/>",$cmtsInfo->description) . "<hr/>";
 							if($cmtsInfo->objectID) { echo $cmtsInfo->objectID . "<hr/>"; }
 							if($cmtsInfo->contact) { echo $cmtsInfo->contact . "<hr/>"; }
-							if($cmtsInfo->location) { echo $cmtsInfo->location . "<hr/>"; }
-							if($cmtsInfo->services) { echo $cmtsInfo->services; }
+							if($cmtsInfo->location) { echo $cmtsInfo->location; }
 						?>
 					</td>
 				</tr>
+			</tbody>
+		</table>
+		
+		<table class="card">
+			<thead class="header">
+				<tr>
+					<th colspan="4">Interfaces</th>
+				</tr>
+			</thead>
+			<tbody>
+				<td colspan="4">
+					<input type="text" id="searchField" onkeypress="filterList()" onkeyup="filterList()" onblur="filterList()" placeholder="Search ..." style="width:98%;padding:8px;border-radius:3px;border:1px solid #bbb" />
+				</td>
+				<?php $indexKey = 'interface.index[]'; $descriptionKey = 'interface.description[]'; 
+					  $adminStatusKey = 'interface.adminStatus[]'; $operationStatusKey = 'interface.operationStatus[]'; $c = 0;
+					foreach($cmtsInterfaces->$indexKey as $cmtsInterfaceKey => $cmtsInterface) { $c++; ?>
+					<?php if($c==1) { echo '<tr>'; } ?>
+						<td class="interface <?php echo strtolower(trim(str_replace("/","-",$cmtsInterfaces->{$descriptionKey}[$cmtsInterfaceKey]))); ?>">
+							<font style="font-size:18px"> <?php echo $cmtsInterfaces->{$descriptionKey}[$cmtsInterfaceKey]; ?> </font> <br/>
+							<font style="font-size:14px"> Admin Status: <?php echo $cmtsInterfaces->{$adminStatusKey}[$cmtsInterfaceKey]; ?> </font> <br/>
+							<font style="font-size:14px"> Operation Status: <?php echo $cmtsInterfaces->{$operationStatusKey}[$cmtsInterfaceKey]; ?> </font>
+						</td>
+					<?php if($c==4) { echo '</tr>'; $c=0; } ?>
+				<?php } ?>
 			</tbody>
 		</table>
 	</div>
@@ -75,6 +109,7 @@ var requestLoop = setInterval(function(){
 		document.getElementById("cmtsCpuUsage").innerHTML = data.cpuUsage;
 		document.getElementById("cmtsTemperatureIn").innerHTML = data.temperatureIn;
 		document.getElementById("cmtsTemperatureOut").innerHTML = data.temperatureOut;
+		document.getElementById("cmtsCountInterfaces").innerHTML = data.countInterfaces;
 	});
 }, 1000);
 function toggleCmtsDetails() {
@@ -84,6 +119,19 @@ function toggleCmtsDetails() {
 	} else {
 		document.getElementById("cmtsDetailsToggler").innerHTML = "▼";
 		document.getElementById("cmtsDetails").style.display = "none";
+	}
+}
+function filterList() {
+	var query = document.getElementById("searchField").value;
+	query = query.trim(); query = query.toLowerCase(); query = query.replace("/","-");
+	var interfaces = document.getElementsByClassName("interface");
+	
+	if(query.length>0) {	
+		for(var i = 0;i<interfaces.length;i++) { interfaces[i].style.display = "none"; }
+		var foundInterfaces = document.querySelectorAll("[class*='interface "+query+"']");
+		for(var j = 0;j<foundInterfaces.length;j++) { foundInterfaces[j].style.display = "table-cell"; }
+	} else {
+		for(var i = 0;i<interfaces.length;i++) { interfaces[i].style.display = "table-cell"; }
 	}
 }
 </script>
