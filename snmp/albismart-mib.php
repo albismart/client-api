a87061de0f91df412fe7140abc3c4b6fa92dd310
arbnorgashi <42361283+arbnorgashi@users.noreply.github.com>
2018-10-11 02:08:14 +0200

    General OIDs and Vendor MIBs added
    Initially adding some mibs for cable modem vendors like cbn, technicolor, thomson.

--- snmp/albismart-mib.php
+++ snmp/albismart-mib.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
 All devices Object ID's from general Mib Tree.
 https://kb.paessler.com/en/topic/653-how-do-snmp-mibs-and-oids-work
 A detailed answer on how SNMP and its MIBs and OIDs work.
@@ -9,9 +9,34 @@ A detailed answer on how SNMP and its MIBs and OIDs work.
 $arris = "1.3.6.1.2.1.1";
 
 return array(
-	"name" => "1.3.6.1.2.1.1.5.0", 
 	"description" => "1.3.6.1.2.1.1.1.0",
+	"objectID" => "1.3.6.1.2.1.1.2.0",
 	"uptime" => "1.3.6.1.2.1.1.3.0",
+	"contact" => "1.3.6.1.2.1.1.4.0",
+	"name" => "1.3.6.1.2.1.1.5.0",
+	"locatioin" => "1.3.6.1.2.1.1.6.0",
+	"services" => "1.3.6.1.2.1.1.7.0",
+	"ifNumber" => "1.3.6.1.2.1.2.1.0",
+	"ifIndex" => "1.3.6.1.2.1.2.2.1.1",
+	"ifDescription" => "1.3.6.1.2.1.2.2.1.2",
+	"ifType" => "1.3.6.1.2.1.2.2.1.3",
+	"ifMtu" => "1.3.6.1.2.1.2.2.1.4",
+	"ifSpeed" => "1.3.6.1.2.1.2.2.1.5",
+	"ifPhysicalAddress" => "1.3.6.1.2.1.2.2.1.6",
+	"ifAdminStatus" => "1.3.6.1.2.1.2.2.1.7",
+	"ifOperationStatus" => "1.3.6.1.2.1.2.2.1.8",
+	"ifLastChange" => "1.3.6.1.2.1.2.2.1.9",
+	"ifInOctets" => "1.3.6.1.2.1.2.2.1.10",
+	"ifInUnicastPackets" => "1.3.6.1.2.1.2.2.1.11",
+	"ifInNotUnicastPackets" => "1.3.6.1.2.1.2.2.1.12",
+	"ifInDiscards" => "1.3.6.1.2.1.2.2.1.13",
+	"ifInErrors" => "1.3.6.1.2.1.2.2.1.14",
+	"ifInUnkownProtos" => "1.3.6.1.2.1.2.2.1.15",
+	"ifOutOctets" => "1.3.6.1.2.1.2.2.1.16",
+	"ifOutUnicastPackets" => "1.3.6.1.2.1.2.2.1.17",
+	"ifOutNotUnicastPackets" => "1.3.6.1.2.1.2.2.1.18",
+	"ifOutDiscards" => "1.3.6.1.2.1.2.2.1.19",
+	"ifOutErrors" => "1.3.6.1.2.1.2.2.1.20",
 );
 
 ?>
