c95637afc58606ee41ba8729b23a15a7cd3f515a
Mentor Gashi <m.gashi@albismart.com>
2019-06-15 21:58:20 +0200

    Move Driver classes out of controller

--- snmp/cablemodem/index.php
+++ snmp/cablemodem/index.php
@@ -5,28 +5,9 @@ $basePath = strstr($basePath, "client-api/", true) . "client-api";
 
 include_once $basePath . "/bootstrap.php";
 include_once snmp_path("/driver.php");
+include_once snmp_path("/cmts/driver.php");
+include_once snmp_path("/cablemodem/driver.php");
 validateApiRequest();
-
-Class CableModem_SNMP_Driver extends SNMP_Driver {
-	public function __construct() {
-		parent::__construct();
-		$cmtsMIB = include_once snmp_path("/cablemodem/vendors/{$this->vendor}.php");
-		$this->mibs = array_merge_recursive_ex($this->mibs, $cmtsMIB);
-	}
-
-	/***********************************************
-	*	linuxIP/snmp/cablemodem/info/{mac}
-	*************************************************/
-	public function info() {
-		$cmtsInfo = array(
-			"about" => $this->read("about"),
-			"stats" => $this->read("stats"),
-		);
-
-		returnJson($cmtsInfo);
-	}
-
-}
 
 $action = (isset($_GET["action"])) ? $_GET["action"] : "info";
 $cableModemSnmpDriver = new CableModem_SNMP_Driver();
