bcc77a5377be31cbde72f3e8d3e6c433d7351597
Mentor Gashi <m.gashi@albismart.com>
2019-06-15 10:24:01 +0200

    Typo

--- snmp/albismart-mib.php
+++ snmp/albismart-mib.php
@@ -1,12 +1,10 @@
 <?php
 
 /*
-All devices Object ID's from general Mib Tree.
-https://kb.paessler.com/en/topic/653-how-do-snmp-mibs-and-oids-work
-A detailed answer on how SNMP and its MIBs and OIDs work.
+AlbiSmart Generic Mib tree.
 */
 
-$generalTree = "1.3.6.1.2.1.1";
+$generalTree = "1.3.6.1.2.1.1;1.3.6.1.2.1.2";
 
 return array(
 	"about" => array(
