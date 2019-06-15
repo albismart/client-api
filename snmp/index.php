3f3ee7276fa52f2e6bc336e6175364d7b57aadd7
Mentor Gashi <m.gashi@albismart.com>
2019-06-15 10:22:25 +0200

    Avoid using SNMP as class name

--- snmp/index.php
+++ snmp/index.php
@@ -7,10 +7,8 @@ include_once $basePath . "/bootstrap.php";
 include_once snmp_path("/driver.php");
 validateApiRequest();
 
-Class SNMP extends SNMP_Driver {}
-
 $action = (isset($_GET["action"])) ? $_GET["action"] : "mibs";
-$snmpDriver = new SNMP();
+$snmpDriver = new SNMP_Driver();
 $snmpDriver->$action();
 
 ?>
